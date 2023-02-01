cffa932456504bf4a8d4804bfc78771c46f663d9
Brock <bpear520@mtroyal.ca>
2023-02-01 09:10:13 -0700

    Commented a bunch of classes.

--- Main.java
+++ Main.java
@@ -1,21 +1,28 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     
     public static void main(String[] args) {
 
+        // Create instances of the MazeBuilder and GraphBuilder classes
         MazeBuilder mazeBuilder = new MazeBuilder();
         GraphBuilder graphBuilder = new GraphBuilder();
-        
+
+        // Use the MazeBuilder instance to build a maze
         Maze maze = mazeBuilder.buildMaze();
+
+        // Use the GraphBuilder instance to build a graph based on the maze
         Graph graph = graphBuilder.buildGraph(maze);
 
         // graph.getGraph().printGraph();
 
+        // Create an instance of the MazeSolver class, passing the maze as a parameter
         MazeSolver solver = new MazeSolver(maze);
 
+        // Find the path from the start node to the end node in the graph
         ArrayList<Node> path = graph.findPath(graph.getStartNode(), graph.getEndNode());
+
+        // Use the MazeSolver instance to build and print the solved maze
         solver.buildPrintMaze(path);
     }
 }
