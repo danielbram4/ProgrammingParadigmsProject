6fcaeef457fc0f48ac840183907b9b11ae581848
Scott Charles <scotty.charles@gmail.com>
2023-02-08 20:31:14 -0700

    old imports deleted

--- Messages.java
+++ Messages.java
@@ -1,7 +1,5 @@
 import java.util.*;
-import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
